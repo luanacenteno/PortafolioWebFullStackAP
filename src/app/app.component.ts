58369756b04f80a511d26629a9ad07cad9cc2593
Luana Centeno <luanacenteno1999@gmail.com>
2022-05-14 13:17:16 -0300

    animation home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { PageScrollService } from 'ngx-page-scroll-core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('closed', style({
+        height: '2vh',
+        widht: '2%',
+        backgroundColor: 'yellow',
+        zIndex: '3',
+        position: 'absolute'
+      })),
+      state('open', style({
+        height: '100vh',
+        width: '100%',
+        backgroundColor: 'yellow',
+        zIndex: '3',
+        position: 'absolute',
+      })),
+      transition('closed <=> open', [
+        animate('1s', )
+      ])
+    ])
+  ]
 })
+
 export class AppComponent {
   title = 'portafolio-front';
+
+  isOpen = false;
+  activeUIIndex: any;
+
+
+  constructor(private pageScrollService: PageScrollService, @Inject(DOCUMENT) private document: any) { 
+    
+  }
+ 
+  toggle() {
+    console.log('toggle clicked', this.isOpen);
+    // event.stopPropagation();
+    this.isOpen = !this.isOpen;
+  }
+  
+  scrollCustomImplementation(element: HTMLElement, index: any) {
+    this.pageScrollService.scroll({
+      document: this.document,
+      scrollTarget: element,
+    });
+    this.activeUIIndex = index;
+    this.toggle();
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core' ; 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxPageScrollCoreModule.forRoot( { duration: 500 } )
   ],
   providers: [],
   bootstrap: [AppComponent]
