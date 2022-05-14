d80e37944d99b6646e4d6620ee6f7e41ac152625
Luana Centeno <luana.centeno99@gmail.com>
2022-05-14 19:03:22 -0300

    Section 2- background 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ import {
         width: '100%',
         opacity: 0,
         zIndex: '0',
-        backgroundColor: 'yellow',
+        backgroundColor: '#F2CB05',
         position: 'absolute'
       })),
       state('open', style({
@@ -26,7 +26,7 @@ import {
         width: '100%',
         opacity: 1,
         zIndex: '3',
-        backgroundColor: 'yellow',
+        backgroundColor: '#F2CB05',
         position: 'absolute',
       })),
       transition('closed <=> open', [
@@ -42,9 +42,60 @@ export class AppComponent implements OnInit {
 
   start: boolean = false
   stringInterpolation: string = "Full Stack Web Developer"
+  section2 = true;
+  section3 = true;
+  section4 = true;
 
   isOpen = false;
   activeUIIndex: any;
+
+  skills: any = [{
+    name: "English",
+    value: 25
+  }, {
+    name: "Angular",
+    value: 45
+  },{
+    name: "PHP",
+    value: 50
+  },{
+    name: "JavaScript",
+    value: 50
+  },{
+    name: "Mysql",
+    value: 55
+  },{
+    name: "Css",
+    value: 85
+  },{
+    name: "HTML",
+    value: 90
+  },{
+    name: "Bootstrap",
+    value: 70
+  },
+];
+
+
+do(){
+  const circles = document.querySelectorAll('.circles');
+  let j = 0
+  circles.forEach(elem =>{
+      let dots: any = elem.getAttribute('data-dots');
+      console.log('dots', dots);
+      let marked = this.skills[j].value;
+      console.log('marked', marked);
+      var percent = Math.floor(dots*marked/100);
+      console.log('percent', percent);
+      var rotate = 360/dots;
+      var points = "";
+      for (let i = 0; i < percent; i++){
+        points += `<div class="points" style="--i: ${i}; --rot: ${rotate}deg"></div>`;
+      }
+      elem.innerHTML = points;
+      j++;
+  });
+}
 
   constructor() { 
     
@@ -54,6 +105,7 @@ export class AppComponent implements OnInit {
     console.log('toggle clicked', this.isOpen);
     // event.stopPropagation();
     this.isOpen = !this.isOpen;
+    this.section2 = true;
   }
   
   scrollCustomImplementation(element: HTMLElement, index: any) {
@@ -72,6 +124,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit () {
+    setTimeout(() =>{
+      this.do();
+    })
     // Start after 1 second + 1 second of startDelay
     // setTimeout(() => this.start = true, 1000)
     // setTimeout(()=> this.toggle(), 2000);
